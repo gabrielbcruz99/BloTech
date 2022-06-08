f17f91ebcc1c5c211065b480ba8ef77cd342126d
gabrielbcruz99 <gabriel.cruz@al.forsoft.org.br>
2022-06-08 12:06:58 -0300

    Crud completo, verificar processo de atualização, pois ao clicar no botao de atualizar, os dados não são enviados para o backend

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -53,8 +53,39 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article: article, categories: categories});
+            })
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
+})
 
+router.post("/articles/update", (req, res) => {
+    var id = req.params.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
 
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    })
+
+})
 
 
 module.exports = router;
